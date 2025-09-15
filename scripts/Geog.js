9b9329608bb8873e2bc2518bda57123ddc7cbbee
Bidhan Acharya <bidhanacharya@mail.weber.edu>
2025-09-14 21:52:11 -0600

    Added the shape calculator

---
+++ scripts/Geog.js
@@ -0,0 +1,23 @@
+function areaFunction() {
+    let base = document.getElementById('base').value;
+    let height = document.getElementById('height').value;
+
+    let result_area = (base * height) /2;
+    document.getElementById('area_result').innerHTML = result_area;
+}
+function perimeterFunction() {
+    let sideA = Number(document.getElementById('sideA').value);
+    let sideB = Number(document.getElementById('sideB').value);
+    let sideC = Number(document.getElementById('sideC').value);
+
+    let result_perimeter = sideA + sideB + sideC;
+    document.getElementById('perimeter_result').innerHTML = result_perimeter;
+}
+function volume() {
+    let length = document.getElementById('length').value;
+    let width = document.getElementById('width').value;
+    let heightB = document.getElementById('heightB').value;
+
+    let result_volume = (length * width * heightB) * 1/3;
+    document.getElementById('volume_result').innerHTML = result_volume;
+}
